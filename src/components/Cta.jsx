9916833e3d0b80af494ed68f6470e0842fca14c9
Harshpreet Singh <harshpreet2972004@gmail.com>
2024-11-24 10:28:22 +0530

    minor changes

--- src/components/Cta.jsx
+++ src/components/Cta.jsx
@@ -3,7 +3,7 @@ import DraftsIcon from "@mui/icons-material/DraftsOutlined";
 
 export const Cta = () => {
   return (
-    <div className="center gap-12 text-para xl:text-[14px] lgl:text-[13px] md:text-[12px] pt-4 font-semibold hide">
+    <div className="center gap-12 text-para lgl:text-[13px] md:text-[12px] pt-4 font-semibold hide">
       <p>
         <PhoneInTalkIcon /> Mon-Fri: 8am-5pm +44 1111 222 333
       </p>
